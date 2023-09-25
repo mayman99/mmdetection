544866f5e69c76c63fb70d70e57a27647a6440e0
Mohamad <no.future2e1@gmail.com>
2023-09-25 17:43:05 +0300

    increase tran epochs to avoid nan in training

--- configs/_base_/datasets/blenderproc_cubes.py
+++ configs/_base_/datasets/blenderproc_cubes.py
@@ -35,8 +35,8 @@ test_pipeline = [
                    'scale_factor'))
 ]
 train_dataloader = dict(
-    batch_size=4,
-    num_workers=4,
+    batch_size=1,
+    num_workers=1,
     persistent_workers=True,
     sampler=dict(type='DefaultSampler', shuffle=True),
     batch_sampler=dict(type='AspectRatioBatchSampler'),
