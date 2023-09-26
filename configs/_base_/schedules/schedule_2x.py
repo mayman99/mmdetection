1947e08aadda921dc6d10bbe099a2fff173abd03
Mohamad <no.future2e1@gmail.com>
2023-09-26 19:51:38 +0300

    inc lr

--- configs/_base_/schedules/schedule_2x.py
+++ configs/_base_/schedules/schedule_2x.py
@@ -1,17 +1,16 @@
 # training schedule for 2x
-max_epochs = 120
-train_cfg = dict(type='EpochBasedTrainLoop', max_epochs=max_epochs, val_interval=500)
-val_cfg = dict(type='ValLoop')
-test_cfg = dict(type='TestLoop')
+train_cfg = dict(type='EpochBasedTrainLoop', max_epochs=2000, val_interval=500)
+# val_cfg = dict(type='ValLoop')
+# test_cfg = dict(type='TestLoop')
 
 # learning rate
 param_scheduler = [
     dict(
-        type='LinearLR', start_factor=0.001, by_epoch=False, begin=0, end=max_epochs),
+        type='LinearLR', start_factor=0.001, by_epoch=False, begin=0, end=500),
     dict(
         type='MultiStepLR',
         begin=0,
-        end=max_epochs,
+        end=2000,
         by_epoch=True,
         milestones=[16, 32],
         gamma=0.1)
@@ -20,7 +19,7 @@ param_scheduler = [
 # optimizer
 optim_wrapper = dict(
     type='OptimWrapper',
-    optimizer=dict(type='SGD', lr=0.002, momentum=0.9, weight_decay=0.0001))
+    optimizer=dict(type='SGD', lr=0.02, momentum=0.9, weight_decay=0.0001))
 
 # Default setting for scaling LR automatically
 #   - `enable` means enable scaling LR automatically
