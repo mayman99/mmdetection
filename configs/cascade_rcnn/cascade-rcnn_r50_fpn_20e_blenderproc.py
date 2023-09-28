277d2febd7ac09a05cbc1edc9e44f3ed00728611
Mohamad <no.future2e1@gmail.com>
2023-09-28 12:39:46 +0300

    validate after each epoch

--- configs/cascade_rcnn/cascade-rcnn_r50_fpn_20e_blenderproc.py
+++ configs/cascade_rcnn/cascade-rcnn_r50_fpn_20e_blenderproc.py
@@ -5,7 +5,7 @@ _base_ = [
 
 # training schedule for 20e
 max_epochs = 100
-train_cfg = dict(type='EpochBasedTrainLoop', max_epochs=max_epochs, val_interval=50)
+train_cfg = dict(type='EpochBasedTrainLoop', max_epochs=max_epochs, val_interval=1)
 val_cfg = dict(type='ValLoop')
 test_cfg = dict(type='TestLoop')
 
