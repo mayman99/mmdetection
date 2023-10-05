60d8742296b32a9a1f3009ef9c3f4e0956d9c1c1
Mohamad <no.future2e1@gmail.com>
2023-10-05 09:01:08 +0300

    inc batchsize

--- configs/_base_/datasets/blenderproc_cubes.py
+++ configs/_base_/datasets/blenderproc_cubes.py
@@ -16,7 +16,7 @@ data_root = 'data/merged_random_cubes/'
 #         'data/': 's3://openmmlab/datasets/detection/'
 #     }))
 backend_args = None
-batch_size_ = 1
+batch_size_ = 8
 train_pipeline = [
     dict(type='LoadImageFromFile', backend_args=backend_args),
     dict(type='LoadAnnotations', with_bbox=True),
